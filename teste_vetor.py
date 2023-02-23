8d59515253b9c8b67afd9c01234cc439b54e4312
Emerson Lourenco <emersonlourenco@iMac-de-Emerson.local>
2023-02-22 21:47:14 -0300

    Copilando Teste Vetor

---
+++ teste_vetor.py
@@ -0,0 +1,13 @@
+
+N: int
+N = int(input("Quantos Numeros voce vai digitar? "))
+vet:[float] = [0 for x in range(N)]
+
+for i in range(0, N):
+    vet[i] = float(input("Digite um numero: "))
+    
+print()
+print("NUMEROS DIGITADOS:")
+for i in range(0, N):
+    print(f"{vet[i]:.1f}")
+    
